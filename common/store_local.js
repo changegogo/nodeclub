0f8eb2a0d1c2f579ba2fdd519b640ac660ce2532
dailliwang <758017188@qq.com>
2018-05-26 11:40:14 +0800

    nodeclub

---
+++ common/store_local.js
@@ -0,0 +1,45 @@
+var config  = require('../config');
+var utility = require('utility');
+var path    = require('path');
+var fs      = require('fs');
+
+exports.upload = function (file, options, callback) {
+  var filename = options.filename;
+
+  var newFilename = utility.md5(filename + String((new Date()).getTime())) +
+    path.extname(filename);
+
+  var upload_path = config.upload.path;
+  var base_url    = config.upload.url;
+  var filePath    = path.join(upload_path, newFilename);
+  var fileUrl     = base_url + newFilename;
+
+  file.on('end', function () {
+    callback(null, {
+      url: fileUrl
+    });
+  });
+
+  file.pipe(fs.createWriteStream(filePath));
+};
+
+
+exports.uploadAvatar = function (file, options, callback) {
+  var filename = options.filename;
+
+  var newFilename = utility.md5(filename + String((new Date()).getTime())) +
+    path.extname(filename);
+
+  var avatar_path = config.avatar.path;
+  var base_url    = config.avatar.url;
+  var filePath    = path.join(avatar_path, newFilename);
+  var fileUrl     = base_url + newFilename;
+
+  file.on('end', function () {
+    callback(null, {
+      url: fileUrl
+    });
+  });
+
+  file.pipe(fs.createWriteStream(filePath));
+};
